761a88fb2e05f7df44b559ed017ec139a15f4df6
RiccardoSalvalajo <salvalajo.riccardo.cartiera@gmail.com>
2021-09-13 16:35:15 +0200

    model + seeder + controller(index)

--- database/migrations/2021_09_13_140455_create_posts_table.php
+++ database/migrations/2021_09_13_140455_create_posts_table.php
@@ -15,6 +15,9 @@ class CreatePostsTable extends Migration
     {
         Schema::create('posts', function (Blueprint $table) {
             $table->id();
+            $table->string('title',100);
+            $table->string('slug')->unique();
+            $table->text('content');
             $table->timestamps();
         });
     }
